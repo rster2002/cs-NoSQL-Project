aa92a4125787e83ac45a554979738164546b2e76
rster20002 <rster2002dev@gmail.com>
2020-09-30 12:04:20 +0200

    Initial commit
    + Created the required projects and a project for testing
    + Setup references for the projects
    + Split connection strings into a seperate file (connections.config)

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole {
+    class Program {
+        static void Main(string[] args) {
+            Program program = new Program();
+            program.Start();
+
+            Console.ReadKey();
+        }
+
+        public void Start() {
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString);
+        }
+    }
+}
