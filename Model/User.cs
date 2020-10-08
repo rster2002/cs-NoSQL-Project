42ee44e765fce013c46b9a7d6817237648fd7073
rster20002 <rster2002dev@gmail.com>
2020-10-08 10:48:16 +0200

    Continued working on basis for the application + Added IntegrityCheck to the User model + Added `Username`, `PasswordHash` and `UserType` to the `User` model + Created the basic structure of the `UserSession` for loggin in

--- Model/User.cs
+++ Model/User.cs
@@ -10,8 +10,22 @@ namespace Model {
     public class User : IEntity {
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public UserType UserType { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+
+        public bool ValidateIntegraty() {
+            if (Id == null) return false;
+            if (Username == null) return false;
+            if (PasswordHash == null) return false;
+            if (Name == null) return false;
+            if (LastName == null) return false;
+            if (Email == null) return false;
+
+            return true;
+        }
     }
 }
---
+++ Service/UserSession.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service {
+    class UserSession {
+        private static UserSession instance;
+
+        public User LoggedInUser { private set; get; }
+
+        private UserSession() { }
+
+        public static UserSession GetInstance() {
+            if (instance == null) instance = new UserSession();
+            return instance;
+        }
+
+        public bool Login(User user, string password) {
+            // TODO: Login and password checking should happen here
+            throw new NotImplementedException();
+        }
+    }
+}
