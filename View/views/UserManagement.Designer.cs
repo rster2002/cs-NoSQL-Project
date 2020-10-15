26fff71561c6e5afa6cbc35444097697a8524e3c
Jordy <jordy.beentjes@gmail.com>
2020-10-15 13:18:54 +0200

    WIP: User management

---
+++ View/views/UserManagement.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace View.views {
+    partial class UserManagement {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.centerPanel = new System.Windows.Forms.Panel();
+            this.lstUsers = new System.Windows.Forms.ListView();
+            this.clId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clFirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clLastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clTickets = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.centerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.Controls.Add(this.centerPanel, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1218, 693);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // centerPanel
+            // 
+            this.centerPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.centerPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerPanel.Controls.Add(this.lstUsers);
+            this.centerPanel.Controls.Add(this.btnAddUser);
+            this.centerPanel.Controls.Add(this.txtSearch);
+            this.centerPanel.Controls.Add(this.label1);
+            this.centerPanel.Location = new System.Drawing.Point(281, 4);
+            this.centerPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.centerPanel.Name = "centerPanel";
+            this.centerPanel.Size = new System.Drawing.Size(656, 689);
+            this.centerPanel.TabIndex = 0;
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clId,
+            this.clEmail,
+            this.clFirstName,
+            this.clLastName,
+            this.clTickets});
+            this.lstUsers.HideSelection = false;
+            this.lstUsers.Location = new System.Drawing.Point(20, 198);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(593, 455);
+            this.lstUsers.TabIndex = 3;
+            this.lstUsers.UseCompatibleStateImageBehavior = false;
+            this.lstUsers.View = System.Windows.Forms.View.Details;
+            // 
+            // clId
+            // 
+            this.clId.Text = "ID";
+            this.clId.Width = 42;
+            // 
+            // clEmail
+            // 
+            this.clEmail.Text = "Email";
+            this.clEmail.Width = 227;
+            // 
+            // clFirstName
+            // 
+            this.clFirstName.Text = "Firstname";
+            this.clFirstName.Width = 123;
+            // 
+            // clLastName
+            // 
+            this.clLastName.Text = "Lastname";
+            this.clLastName.Width = 131;
+            // 
+            // clTickets
+            // 
+            this.clTickets.Text = "# tickets";
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAddUser.FlatAppearance.BorderColor = System.Drawing.Color.GreenYellow;
+            this.btnAddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddUser.Location = new System.Drawing.Point(458, 95);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(155, 38);
+            this.btnAddUser.TabIndex = 2;
+            this.btnAddUser.Text = "+ ADD NEW USER";
+            this.btnAddUser.UseVisualStyleBackColor = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtSearch.Location = new System.Drawing.Point(20, 95);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(251, 38);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.Text = "Filter by email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Management";
+            // 
+            // UserManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "UserManagement";
+            this.Size = new System.Drawing.Size(1218, 693);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.centerPanel.ResumeLayout(false);
+            this.centerPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel centerPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListView lstUsers;
+        private System.Windows.Forms.ColumnHeader clId;
+        private System.Windows.Forms.ColumnHeader clEmail;
+        private System.Windows.Forms.ColumnHeader clFirstName;
+        private System.Windows.Forms.ColumnHeader clLastName;
+        private System.Windows.Forms.ColumnHeader clTickets;
+    }
+}
