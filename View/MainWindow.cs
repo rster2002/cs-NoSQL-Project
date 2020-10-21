3c12ec5b064a5b68def1bb3e02b692929ff2c55b
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2020-10-21 16:10:03 +0200

    [Topbar] Fix multi user sessions

--- View/MainWindow.cs
+++ View/MainWindow.cs
@@ -37,10 +37,12 @@ namespace View {
             usersToolStripMenuItem.Visible = loggedInUserIsEditor;
             ArchiveToolStripMenuItem.Visible = loggedInUserIsEditor;
             mainMenuStrip.Visible = show;
+            lbl_userName.Visible = show;
 
-            lbl_userName.Text = $"Welcome, {userSession.LoggedInUser.Name}";
-            lbl_userName.Show();
-            mainMenuStrip.Left = Width - mainMenuStrip.Width - 20;
+            if (show) {
+                lbl_userName.Text = $"Welcome, {userSession.LoggedInUser.Name}";
+                mainMenuStrip.Left = Width - mainMenuStrip.Width - 20;
+            }
         }
 
         private void TicketsToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -63,6 +65,7 @@ namespace View {
             userSession.Logout();
 
             mainMenuStrip.Visible = false;
+            lbl_userName.Visible = false;
             LoadView(new LoginView(this));
         }
     }
