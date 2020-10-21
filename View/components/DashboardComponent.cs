52da608db715abc11e56575044aef36399757920
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2020-10-21 17:27:21 +0200

    Add the stupid circle to the dashboard

--- View/components/DashboardComponent.cs
+++ View/components/DashboardComponent.cs
@@ -32,10 +32,14 @@ namespace View.components {
             unsolvedTickets = tickets.Count(x => x.OpenStatus != OpenState.Closed);
             overdueTickets = tickets.Count(x => x.OpenStatus != OpenState.Closed && x.DueDate < DateTime.Now);
 
-            chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
-            chart1.Series[0].Points.AddXY("0", unsolvedTickets);
-            chart1.Series[1].Points.AddXY("0", overdueTickets);
-            chart1.ChartAreas[0].AxisY.Maximum = totalTickets;
+            //chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            //chart1.Series[0].Points.AddXY(unsolvedTickets, unsolvedTickets);
+            //chart1.Series[1].Points.AddXY(overdueTickets, overdueTickets);
+            //chart1.ChartAreas[0].AxisY.Maximum = totalTickets;
+
+            chart1.Series[0].Points[0].YValues = new double[] { totalTickets - unsolvedTickets };
+            chart1.Series[0].Points[1].YValues = new double[] { unsolvedTickets - overdueTickets };
+            chart1.Series[0].Points[2].YValues = new double[] { overdueTickets };
 
             lbl_info.Text = $"Total tickets: {totalTickets}\nTotal Unsolved tickets: {unsolvedTickets}\nTotal overdue tickets: {overdueTickets}";
         }
