5ec306ccb17d5974828201cb2281ac9446d65887
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2020-10-21 16:40:35 +0200

    Edit UserManagement [Needs discussion]

--- View/views/UserManagement.Designer.cs
+++ View/views/UserManagement.Designer.cs
@@ -23,7 +23,6 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.centerPanel = new System.Windows.Forms.Panel();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -34,45 +33,27 @@
             this.clLastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clTickets = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnAddUser = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
             this.centerPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.centerPanel, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(895, 706);
-            this.tableLayoutPanel1.TabIndex = 1;
-            // 
             // centerPanel
             // 
-            this.centerPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.centerPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.centerPanel.Controls.Add(this.txtSearch);
             this.centerPanel.Controls.Add(this.label2);
             this.centerPanel.Controls.Add(this.lstUsers);
             this.centerPanel.Controls.Add(this.btnAddUser);
-            this.centerPanel.Controls.Add(this.label1);
-            this.centerPanel.Location = new System.Drawing.Point(201, 142);
+            this.centerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.centerPanel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.centerPanel.Location = new System.Drawing.Point(0, 0);
             this.centerPanel.Name = "centerPanel";
-            this.centerPanel.Size = new System.Drawing.Size(492, 422);
+            this.centerPanel.Size = new System.Drawing.Size(853, 350);
             this.centerPanel.TabIndex = 0;
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(15, 116);
+            this.txtSearch.Location = new System.Drawing.Point(164, 0);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(190, 20);
+            this.txtSearch.Size = new System.Drawing.Size(262, 20);
             this.txtSearch.TabIndex = 0;
             this.txtSearch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtSearch_MouseClick);
             this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
@@ -82,14 +63,17 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 81);
+            this.label2.Location = new System.Drawing.Point(3, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.Size = new System.Drawing.Size(155, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Filter by email";
+            this.label2.Text = "Filter Users by email:";
             // 
             // lstUsers
             // 
+            this.lstUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lstUsers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.clId,
             this.clEmail,
@@ -98,11 +82,11 @@
             this.clTickets});
             this.lstUsers.FullRowSelect = true;
             this.lstUsers.HideSelection = false;
-            this.lstUsers.Location = new System.Drawing.Point(2, 160);
+            this.lstUsers.Location = new System.Drawing.Point(3, 26);
             this.lstUsers.Margin = new System.Windows.Forms.Padding(2);
             this.lstUsers.MultiSelect = false;
             this.lstUsers.Name = "lstUsers";
-            this.lstUsers.Size = new System.Drawing.Size(486, 258);
+            this.lstUsers.Size = new System.Drawing.Size(849, 322);
             this.lstUsers.TabIndex = 3;
             this.lstUsers.UseCompatibleStateImageBehavior = false;
             this.lstUsers.View = System.Windows.Forms.View.Details;
@@ -116,57 +100,46 @@
             // clEmail
             // 
             this.clEmail.Text = "Email";
-            this.clEmail.Width = 122;
+            this.clEmail.Width = 164;
             // 
             // clFirstName
             // 
             this.clFirstName.Text = "Firstname";
-            this.clFirstName.Width = 110;
+            this.clFirstName.Width = 183;
             // 
             // clLastName
             // 
             this.clLastName.Text = "Lastname";
-            this.clLastName.Width = 126;
+            this.clLastName.Width = 166;
             // 
             // clTickets
             // 
             this.clTickets.Text = "# tickets";
+            this.clTickets.Width = 75;
             // 
             // btnAddUser
             // 
-            this.btnAddUser.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnAddUser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
             this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnAddUser.FlatAppearance.BorderColor = System.Drawing.Color.GreenYellow;
-            this.btnAddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAddUser.Location = new System.Drawing.Point(344, 77);
+            this.btnAddUser.Location = new System.Drawing.Point(737, 0);
             this.btnAddUser.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(116, 31);
+            this.btnAddUser.Size = new System.Drawing.Size(116, 20);
             this.btnAddUser.TabIndex = 2;
             this.btnAddUser.Text = "+ ADD NEW USER";
             this.btnAddUser.UseVisualStyleBackColor = false;
             this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 28);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "User Management";
-            // 
             // UserManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.centerPanel);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UserManagement";
-            this.Size = new System.Drawing.Size(895, 706);
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(853, 350);
             this.centerPanel.ResumeLayout(false);
             this.centerPanel.PerformLayout();
             this.ResumeLayout(false);
@@ -175,17 +148,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel centerPanel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListView lstUsers;
         private System.Windows.Forms.ColumnHeader clId;
         private System.Windows.Forms.ColumnHeader clEmail;
         private System.Windows.Forms.ColumnHeader clFirstName;
         private System.Windows.Forms.ColumnHeader clLastName;
         private System.Windows.Forms.ColumnHeader clTickets;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnAddUser;
     }
 }
